86d89a3b4ea99e966ab32b60f900fd55730da62c
nikeshmhr <nikeshmhr@gmail.com>
2025-08-26 21:45:41 +0400

    feat: typescript and dev env setup

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+export default function cli(): void;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export default function cli() {
+  console.log("Task Tracker CLI - Typescript - with watch mode - 5");
+}
